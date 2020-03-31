9b1ad79658c9f1ecb9f00850605aaf154879efd1
Valtteri Laine <valtteri.laine@takamaki.fi>
2020-03-31 19:19:37 +0000

    adds prod and dev firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,16 +3,32 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/analytics';
 
-export const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'pain-lessons.firebaseapp.com',
-  databaseURL: 'https://pain-lessons.firebaseio.com',
-  projectId: 'pain-lessons',
-  storageBucket: 'pain-lessons.appspot.com',
-  messagingSenderId: '505852011185',
-  appId: '1:505852011185:web:f0606da1a38a56a95f7890',
-  measurementId: 'G-SGWKBKMV75',
-};
+export let config;
+
+if (process.env.REACT_APP_ENVIRONMENT === 'DEVELOPMENT') {
+  config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: 'simple-reflect-dev-34813.firebaseapp.com',
+    databaseURL: 'https://simple-reflect-dev-34813.firebaseio.com',
+    projectId: 'simple-reflect-dev-34813',
+    storageBucket: 'simple-reflect-dev-34813.appspot.com',
+    messagingSenderId: '809756491978',
+    appId: '1:809756491978:web:158a9f212c0c74c6f1b433',
+    measurementId: 'G-20YXD6RDC6',
+  };
+} else if (process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
+  config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: 'simple-reflect-8559d.firebaseapp.com',
+    databaseURL: 'https://simple-reflect-8559d.firebaseio.com',
+    projectId: 'simple-reflect-8559d',
+    storageBucket: 'simple-reflect-8559d.appspot.com',
+    messagingSenderId: '135611553354',
+    appId: '1:135611553354:web:b315a463fce22672e3f8b3',
+    measurementId: 'G-1P1PD89FYW',
+  };
+}
+
 
 firebase.initializeApp(config);
 
