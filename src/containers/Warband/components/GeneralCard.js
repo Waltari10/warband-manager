80d6f573b8ef241a13e1baa593e34f1951bf4258
Valtteri Laine <valtteri.laine@takamaki.fi>
2020-05-03 12:30:01 +0100

    adds autoComplete warband type

--- src/containers/Warband/components/GeneralCard.js
+++ src/containers/Warband/components/GeneralCard.js
@@ -1,6 +1,14 @@
 import React, { memo } from 'react';
-
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import { Grid, TextField } from '@material-ui/core';
+
+const warbandTypes = [
+  'Cult of the Possessed',
+  'The Sisters of Sigmar',
+  'Witch Hunters',
+  'The undead',
+  'Skaven',
+];
 
 const GeneralCard = memo(({ classes, name, type, gamesPlayed, handleChange }) => {
   return (
@@ -12,8 +20,6 @@ const GeneralCard = memo(({ classes, name, type, gamesPlayed, handleChange }) =>
         id="general_header"
         className={classes.h5}
         variant="h5">General</h5>
-
-
       <Grid
         container
         spacing={2}
@@ -30,13 +36,29 @@ const GeneralCard = memo(({ classes, name, type, gamesPlayed, handleChange }) =>
           />
         </Grid>
         <Grid item>
-          <TextField
-            variant="outlined"
-            name="type"
+
+          <Autocomplete
+            selectOnFocus
             value={type || ''}
-            onChange={handleChange}
-            className={classes.textFieldLong}
-            label={'Warband type'}
+            freeSolo
+            clearOnBlur
+            name="type"
+            options={warbandTypes}
+            style={{ width: 300 }}
+            onChange={(event, newValue) => {
+              handleChange({ target: { value: newValue, getAttribute: () => 'type' } });
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                name="type"
+                value={type || ''}
+                onChange={handleChange}
+                label="Warband type"
+                variant="outlined"
+                className={classes.textFieldLong}
+              />
+            )}
           />
         </Grid>
         <Grid item>
